a50250415fb4a3a0b3c76babecdf6310f342ee26
LLarbi <larbi_lachgare@hotmail.fr>
2023-01-26 21:57:09 +0100

    servlet list users

---
+++ src/main/java/com/pie/planingjakartaee/servlet/ListUserServlet.java
@@ -0,0 +1,25 @@
+package com.pie.planingjakartaee.servlet;
+
+import com.pie.planingjakartaee.dao.DaoFactory;
+import com.pie.planingjakartaee.dao.UserDao;
+import com.pie.planingjakartaee.dao.entity.User;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(urlPatterns = "/users")
+public class ListUserServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        UserDao dao = DaoFactory.getUserDao();
+        List<User> userList = dao.getAll();
+        req.setAttribute("users", userList);
+        req.getRequestDispatcher("/WEB-INF/users-list.jsp").forward(req, resp);
+
+    }
+}
