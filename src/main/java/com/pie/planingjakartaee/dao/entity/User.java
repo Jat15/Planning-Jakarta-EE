447388a3ab0d281aada2fde3b689a32c6d248ce3
Jat15 <jinan15@hotmail.com>
2023-01-26 15:40:03 +0100

    [Entity] Create User

--- src/main/java/com/pie/planingjakartaee/dao/entity/User.java
+++ src/main/java/com/pie/planingjakartaee/dao/entity/User.java
@@ -1,4 +1,185 @@
 package com.pie.planingjakartaee.dao.entity;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name="users")
 public class User {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(name="user_id")
+    private int id;
+    @Column(length = 50)
+    private String pseudo;
+    @Column(length = 75)
+    private String email;
+    @Column(name="last_name", length = 50)
+    private String lastName;
+    @Column(name="first_name", length = 50)
+    private String firstName;
+    @Column(length = 50)
+    private String avatar;
+    private LocalDate birthdate;
+    @Column(length = 15)
+    private String phone;
+    private String password;
+
+    @Column(name = "created_date")
+    private LocalDateTime createdDate;
+    @Column(name = "modify_date")
+    private LocalDateTime modifyDate;
+    private boolean activate;
+    @Column(length = 100)
+    private String street;
+    @Column(length = 50)
+    private String city;
+    @Column(length = 50)
+    private String country;
+    @Column(length = 32)
+    private String zip;
+    @ManyToOne
+    private Role role;
+    public User(){
+
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public LocalDateTime getModifyDate() {
+        return modifyDate;
+    }
+
+    public void setModifyDate(LocalDateTime modifyDate) {
+        this.modifyDate = modifyDate;
+    }
+
+    public boolean isActivate() {
+        return activate;
+    }
+
+    public void setActivate(boolean activate) {
+        this.activate = activate;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
 }
