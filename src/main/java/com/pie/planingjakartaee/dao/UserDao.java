650067b4ac8398900923a7f476a549507d867bf1
Jat15 <jinan15@hotmail.com>
2023-01-26 16:02:21 +0100

    [DAO] Create User

---
+++ src/main/java/com/pie/planingjakartaee/dao/UserDao.java
@@ -0,0 +1,52 @@
+package com.pie.planingjakartaee.dao;
+
+import com.pie.planingjakartaee.dao.entity.User;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Persistence;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class UserDao implements Dao<User> {
+
+    private EntityManager em;
+    public UserDao() {
+        em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+    }
+
+    @Override
+    public Optional<User> get(int id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<User> getAll() {
+        List<User> users = new ArrayList<>();
+
+        try {
+            users =  em.createQuery("select p from User p", User.class).getResultList();
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            em.close();
+        }
+
+        return users;
+    }
+
+    @Override
+    public void create(User user) {
+
+    }
+
+    @Override
+    public void update(User user) {
+
+    }
+
+    @Override
+    public void delete(User user) {
+
+    }
+}
