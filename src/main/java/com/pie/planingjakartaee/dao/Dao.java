2783c7581ba5c47baec48d3b8385bf1ed8bf8806
Jat15 <jinan15@hotmail.com>
2023-01-26 16:01:46 +0100

    [DAO] Update DAO interface

--- src/main/java/com/pie/planingjakartaee/dao/Dao.java
+++ src/main/java/com/pie/planingjakartaee/dao/Dao.java
@@ -1,4 +1,12 @@
 package com.pie.planingjakartaee.dao;
 
-public interface Dao {
+import java.util.List;
+import java.util.Optional;
+
+public interface Dao<T> {
+    Optional<T> get(int id);
+    List<T> getAll();
+    void create(T t);
+    void update(T t);
+    void delete(T t);
 }
