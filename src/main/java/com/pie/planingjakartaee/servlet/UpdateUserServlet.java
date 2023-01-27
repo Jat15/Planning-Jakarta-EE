87bb579585e78685ac00c44b0b096c0cec0cfc73
Jat15 <jinan15@hotmail.com>
2023-01-27 17:16:37 +0100

    [Servlet] Fix Update User
    - Url patterns

--- src/main/java/com/pie/planingjakartaee/servlet/UpdateUserServlet.java
+++ src/main/java/com/pie/planingjakartaee/servlet/UpdateUserServlet.java
@@ -11,7 +11,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Optional;
 
-@WebServlet( urlPatterns = "user/update")
+@WebServlet( urlPatterns = "/user/update")
 public class UpdateUserServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
