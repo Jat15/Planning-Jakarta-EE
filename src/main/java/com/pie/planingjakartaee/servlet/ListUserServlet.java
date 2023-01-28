d9c3c21b2c5d98caef998bd2a633e558ac387b4c
Jat15 <jinan15@hotmail.com>
2023-01-28 19:44:23 +0100

    [Servlet] Update List User
    - Update for protect page List User

--- src/main/java/com/pie/planingjakartaee/servlet/ListUserServlet.java
+++ src/main/java/com/pie/planingjakartaee/servlet/ListUserServlet.java
@@ -8,18 +8,33 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 @WebServlet(urlPatterns = "/users")
 public class ListUserServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        UserDao dao = DaoFactory.getUserDao();
-        List<User> userList = dao.getAll();
-        req.setAttribute("users", userList);
-        req.getRequestDispatcher("/WEB-INF/usersList.jsp").forward(req, resp);
+        Boolean accesPage = false;
+        HttpSession session = req.getSession();
 
+        User user = (User) session.getAttribute("user");
+
+        if (user != null){
+            accesPage = user.getRole().getId() > 1;
+        }
+
+        if (accesPage) {
+            UserDao dao = DaoFactory.getUserDao();
+            List<User> userList = dao.getAll();
+
+            req.setAttribute("users", userList);
+            req.getRequestDispatcher("/WEB-INF/usersList.jsp").forward(req, resp);
+        }else {
+            resp.sendRedirect("/");
+        }
     }
 }
