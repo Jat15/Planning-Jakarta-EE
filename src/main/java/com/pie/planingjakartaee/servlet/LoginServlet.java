9a62e40f79376ea93895c1ada3d8d8343dc59528
Jat15 <jinan15@hotmail.com>
2023-01-28 19:42:37 +0100

    [Servlet] Create Login

---
+++ src/main/java/com/pie/planingjakartaee/servlet/LoginServlet.java
@@ -0,0 +1,50 @@
+package com.pie.planingjakartaee.servlet;
+
+import com.pie.planingjakartaee.dao.DaoFactory;
+import com.pie.planingjakartaee.dao.entity.User;
+import jakarta.persistence.Entity;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@WebServlet(urlPatterns = "/")
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/login.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        boolean connected = false;
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+
+        Optional<User> user = DaoFactory.getUserDao().getPassword(email);
+
+        HttpSession session = req.getSession();
+
+        if (user.isPresent()) {
+            //verify password
+            if (password.equals(user.get().getPassword())){
+                connected = true;
+            }
+        }
+
+        if (connected) {
+            User newUser = user.get();
+            session.setAttribute("user", newUser);
+
+            resp.sendRedirect("/users");
+        } else {
+            resp.sendRedirect("/");
+        }
+    }
+}
