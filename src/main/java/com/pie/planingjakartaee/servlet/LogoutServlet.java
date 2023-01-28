34ab7d0ebb3c4c2793f4130bd79ae3bcffa31be4
Jat15 <jinan15@hotmail.com>
2023-01-28 19:41:30 +0100

    [Servlet] Create Logout

---
+++ src/main/java/com/pie/planingjakartaee/servlet/LogoutServlet.java
@@ -0,0 +1,20 @@
+package com.pie.planingjakartaee.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/logout")
+public class LogoutServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        session.invalidate();
+        resp.sendRedirect("/");
+    }
+}
