7ba69db14f4c47ed214e44bcd88b849f773ec182
Jat15 <jinan15@hotmail.com>
2023-02-01 14:52:33 +0100

    [Filter] Create Auth Filter

---
+++ src/main/java/com/pie/planingjakartaee/filter/AuthFilter.java
@@ -0,0 +1,43 @@
+package com.pie.planingjakartaee.filter;
+
+import com.pie.planingjakartaee.dao.entity.User;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {"/users","/user/*"})
+public class AuthFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+
+        HttpSession session = req.getSession();
+        User sessionUser = (User) session.getAttribute("user");
+
+        boolean acces = false;
+        if (sessionUser != null ) {
+            acces = sessionUser.getRole().getId() > 1;
+        }
+
+        if (acces) {
+            // Filtrage OK
+            chain.doFilter(request, response);
+        } else {
+            resp.sendRedirect("/");
+        }
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
