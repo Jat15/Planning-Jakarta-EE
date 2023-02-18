2143dac0928c0d1f41bd3a811e941767a78e7a4b
Jat15 <jinan15@hotmail.com>
2023-02-19 00:46:50 +0100

    [Servlet] Update UserDelete
    - Convert errors boolean > list
    - Add verify and cut all error.

--- src/main/java/com/pie/planingjakartaee/servlet/DeleteUserServlet.java
+++ src/main/java/com/pie/planingjakartaee/servlet/DeleteUserServlet.java
@@ -11,35 +11,69 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @WebServlet(urlPatterns = "/user/delete")
 public class DeleteUserServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        boolean noErrors = true;
+        List<String> listErrors = new ArrayList<>();
         HttpSession session = req.getSession();
         User sessionUser = (User) session.getAttribute("user");
 
         String idStr = req.getParameter("id");
         int sessionIdRole = sessionUser.getRole().getId();
 
+        int id = 0;
+
         try {
-            int id = Integer.parseInt(idStr);
-            UserDao dao = DaoFactory.getUserDao();
-            Optional<User> user = dao.get(id);
+            id = Integer.parseInt(idStr);
+        } catch (Exception e) {
+            System.err.println("Invalid ID : " + e );
+            listErrors.add("Unable to parse integer for invalid ID");
+        }
 
-            if (user.isPresent()) {
-                int idRole = user.get().getRole().getId();
-                noErrors = idRole > 0 && idRole < sessionIdRole;
-
-                if (noErrors) {
-                    UserDao dao1 = DaoFactory.getUserDao();
-                    dao1.delete(user.get());
-                }
+        Optional<User> user = Optional.empty();
+        if (listErrors.size() == 0) {
+            try {
+                UserDao dao = DaoFactory.getUserDao();
+                user = dao.get(id);
+            } catch (Exception e) {
+                System.err.println("Unable to access the database :" + e);
+                listErrors.add("Unable to access the database");
             }
-        } catch (NumberFormatException e) {
-            System.err.println(e.getMessage());
+        }
+
+        if (listErrors.size() == 0) {
+            if (user.isEmpty()) {
+                System.err.println("No user found with the given ID");
+                listErrors.add("No user found with the given ID");
+            }
+        }
+
+        if (listErrors.size() == 0) {
+            int idRole = user.get().getRole().getId();
+            if (!(idRole > 0 && idRole < sessionIdRole)) {
+                System.err.println("You are not authorized to delete this user");
+                listErrors.add("You are not authorized to delete this user");
+            }
+        }
+
+
+        if (listErrors.size() == 0) {
+            try {
+                UserDao dao1 = DaoFactory.getUserDao();
+                dao1.delete(user.get());
+            } catch (Exception e) {
+                listErrors.add("Unable to delete this user");
+                System.err.println("Unable to delete this user : " + e);
+            }
+        }
+
+        if (!listErrors.isEmpty()) {
+            session.setAttribute("errors", listErrors);
         }
 
         resp.sendRedirect(req.getContextPath() + "/users");
